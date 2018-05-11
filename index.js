fbffde1c87834a970453cfa9394232fbb2b4e066
devericx <devericx@gmail.com>
2018-05-11 09:23:43 -0400

    Implement Redux

--- index.js
+++ index.js
@@ -11,12 +11,10 @@ mongoose.connect(keys.mongoURI);
 const app = express();
 
 // set up cookies
-app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
-  })
-);
+app.use(cookieSession({
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+  keys: [keys.cookieKey]
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
