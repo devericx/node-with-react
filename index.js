5319456bfabad3b10cd567eea6900257bea3c774
devericx <devericx@gmail.com>
2018-08-05 13:39:52 -0400

    Implement Stripe on server

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 const keys = require("./config/keys");
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 5000;
 
@@ -10,7 +11,8 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-// set up cookies
+// set up middleware
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -28,5 +30,6 @@ require("./services/passport");
 
 // set up routes
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 
 app.listen(PORT);
